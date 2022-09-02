ba8ff4bb48c6b7fabc3ebc6951f3d76a67847acd
Krausweln <rizqimaulana442@gmail.com>
2022-09-02 20:17:57 +0700

    bagaimana kamu lahir

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,14 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sansita': ['Sansita', 'sans-serif'],
-      }
+        sansita: ["Sansita", "sans-serif"],
+      },
+      colors: {
+        kelahiranBg: "#FDF4F5",
+        kelahiranMain: "#EDD1D2",
+        textMain: "#7C1A1F",
+      },
     },
   },
   plugins: [],
-}
+};
