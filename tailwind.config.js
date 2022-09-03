b0e91c563a0f2d00a53cccbe3d6dbe78fa220b99
Azam Din Abdillah <azzamdinabdillah123@gmail.com>
2022-09-03 16:48:14 +0700

    Excaria

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,15 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sansita': ['Sansita', 'sans-serif'],
-      }
+        sansita: ["Sansita", "sans-serif"],
+      },
+      colors: {
+        kelahiranBg: "#FDF4F5",
+        kelahiranMain: "#EDD1D2",
+        excariaBg: "#E19488",
+        textMain: "#7C1A1F",
+      },
     },
   },
   plugins: [],
-}
+};
